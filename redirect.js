56314ceb5a043e0add9a7f82aca836405809ebb4
DJJoschi <djjoschi441@gmail.com>
2022-10-19 22:32:38 +0200

    Changed the way the extension works

--- redirect.js
+++ redirect.js
@@ -1,1 +1,8 @@
-window.location.href = "https://www.youtube.com/watch?v=" + window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
+function run(){
+    window.location.href = "https://www.youtube.com/watch?v=" + window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
+    console.log('change');
+    }
+    
+    window.onload = run;
+    window.addEventListener('yt-navigate-start', run,   true);
+
