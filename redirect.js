e09eb8e01a8a9daafd4850e984d4a89eebf23b04
DJJoschi <djjoschi441@gmail.com>
2022-10-19 22:47:17 +0200

    Changed the way it redirects

--- redirect.js
+++ redirect.js
@@ -1,9 +1,9 @@
 function run(){
     if(window.location.href.includes("shorts") && window.location.href.charAt(window.location.href.length) != "/")
-        window.location.href = "https://www.youtube.com/watch?v=" + window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
+        window.location.replace("https://www.youtube.com/watch?v=" + window.location.href.substring(window.location.href.lastIndexOf("/") + 1));
     console.log('change');
     }
     
     window.onload = run;
-    window.addEventListener('yt-navigate-start', run,   true);
+    window.addEventListener('yt-navigate-finish', run,   true);
 
